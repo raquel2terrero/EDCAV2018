25900299c7eaf9c5a84f2f5e31b02efcd84ea6ac
Sergio <gio8.tisu@gmail.com>
2018-05-02 23:11:49 +0200

    features

--- main.m
+++ main.m
@@ -1,6 +1,11 @@
 %% read train files
-[fileName, singer] = textread('trainListFile.txt', '%s %s', 'delimiter', '\t');
-N = length(singer);
+[fileNames, singers] = textread('trainListFile.txt', '%s %s', 'delimiter', '\t');
+N = length(singers);
+
+features = cell(N, 2);
 for i=1:N
-    
+    [audioIn, fs] = audioread(fileNames{i});
+    [f0, coef] = extract_feat(audioIn, fs);
+    features{i,1} = [f0, coef];
+    features{i,2} = clase{i};
 end
