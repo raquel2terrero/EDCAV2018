8ded0b65ccc10bf72561eefa2a8797e1f407d6f4
Raquel <raquel2terrero@gmail.com>
2018-05-03 01:32:45 +0200

    main

--- main.m
+++ main.m
@@ -1,27 +1,29 @@
 %% read train files
 [fileNames, singers] = textread('trainListFile.txt', '%s %s', 'delimiter', '\t');
-[singer, clase] = textread('singer_class.txt', '%s %s', 'delimiter', '\t');
+[singer, clase] = textread('singer_class.txt', '%s %d', 'delimiter', '\t');
 N = length(singers);
-clases = cell(N,1);
+clases = zeros(N,1);
 k = 1;
-%sabiendo que estan ordenados
+%vector con clase de cada audio (sabiendo que la lista esta ordenada)
 for i = 1:N
     if strcmp(singers{i},singer{k})==0
         k = k + 1;
     end
-    clases{i} = clase{k};    
+    clases(i) = clase(k);    
 end
 
-features = cell(N, 1); %{class, [pitch, coef]}
+%% extract features for each audio file
+features = cell(N, 2); %{class, [pitch, coef]}
 for i=1:N
     [audioIn, fs] = audioread(fileNames{i});
     [f0, coef] = extract_feat(audioIn, fs);
-    features{i} = [f0, coef];
+    features{i, 1} = clases(i);
+    features{i, 2} = [f0, coef];
 end
 % normalizar
-featureVectors = features{:};
+featureVectors = features{:,2};
 m = mean(featureVectors);
 s = std(featureVectors);
 for i=1:N
-    features{i,2} =  (features{i,2}-m)./s;
+    features{i,2} = (features{i}-m)./s;
 end
